7394d51a85c413e6c08f65414664e8e489fbbf10
DrCsquared <47128307+DrCsquared@users.noreply.github.com>
2019-09-09 16:53:50 -0400

    Initial level 1 layout
    Setting up level 1 layout

---
+++ Tilt/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Click_StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Click_Exit()
+    {
+        Application.Quit();
+    }
+}
+
