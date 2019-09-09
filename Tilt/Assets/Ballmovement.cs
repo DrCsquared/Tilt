b7d62090a6bf8a9437785181f42765bcf8104879
DrCsquared <47128307+DrCsquared@users.noreply.github.com>
2019-09-09 17:12:42 -0400

    Moved the Ball
    This is for testing on PC. Later i'm going to remove this when i want to test on my phone.

---
+++ Tilt/Assets/Ballmovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballmovement : MonoBehaviour
+{
+    [SerializeField]
+    private int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var up_and_down = Input.GetAxis("Vertical");
+        var left_and_right = Input.GetAxis("Horizontal");
+
+        this.transform.Translate(left_and_right * speed * Time.deltaTime, 0f , up_and_down * speed * Time.deltaTime);
+    }
+}
