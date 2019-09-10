3248d6ce48ad161c06f5c7ba5613596df922d411
DrCsquared <47128307+DrCsquared@users.noreply.github.com>
2019-09-10 10:27:57 -0400

    Added Materials & Test movement
    I added some Materials and changed the way i was going to move the shpere testing my code.

--- Tilt/Assets/Ballmovement.cs
+++ Tilt/Assets/Ballmovement.cs
@@ -6,18 +6,34 @@ public class Ballmovement : MonoBehaviour
 {
     [SerializeField]
     private int speed;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+    /*
     // Update is called once per frame
     void Update()
     {
         var up_and_down = Input.GetAxis("Vertical");
         var left_and_right = Input.GetAxis("Horizontal");
 
-        this.transform.Translate(left_and_right * speed * Time.deltaTime, 0f , up_and_down * speed * Time.deltaTime);
+        this.transform.Translate(left_and_right * speed * Time.deltaTime, up_and_down * speed * Time.deltaTime, 0f);
     }
+    */
+
 }
