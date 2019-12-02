f822a247d67767f1d634e7da57ae0bd0a2b56d81
DrCsquared <47128307+DrCsquared@users.noreply.github.com>
2019-12-02 11:22:43 -0500

    Added Level buttons

--- Tilt/Assets/Scripts/Menu Script/MenuScript.cs
+++ Tilt/Assets/Scripts/Menu Script/MenuScript.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    public void Click_StartGame()
+
+    public void LoadLevel1()
     {
         SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level 4");
     }
 
     public void Click_Exit()
